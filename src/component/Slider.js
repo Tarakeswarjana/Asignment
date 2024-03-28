d3ebc437f3c47febcb6cefdecfe30269acd79490
Udayan2000 <ghosh.udayan2000@gmail.com>
2024-03-28 22:38:18 +0530

    up

--- src/component/Slider.js
+++ src/component/Slider.js
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
+// import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 const img1 = require("../assets/Mask Group 99.jpg")
@@ -15,27 +15,29 @@ function Slider() {
     return (
         // <div className='w-screen'>
 
+        <div className="swiperowndiv">
         <Swiper
             className='relative'
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            modules={[Pagination, Scrollbar, A11y]}
             spaceBetween={10}
             slidesPerView={1}
             navigation
             pagination={{
-                clickable: true, renderBullet: function (index, className) {
-                    return (
-                        `<span class="DotClass">
-                    <span class="custom-dot">hhh</span> 
-                  </span>`
-                    )
-                }
+                clickable: true, 
+                // renderBullet: function (index, className) {
+                //     return (
+                //         `<span class="DotClass">
+                //     <span class="custom-dot">hhh</span> 
+                //   </span>`
+                //     )
+                
             }}
 
             // scrollbar={{ draggable: true }}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
         >
-            <SwiperSlide>  <div className='flex gap-3 justify-between bg-[#bab8b8]'><p>jhfjyrekfhkjhewuywiuykejhgejhgiyeiuwrhgkheguyugyerkhgkerjhkj</p><img src={img1} className='bg-[#bab8b8]' /> </div>        </SwiperSlide>
+            <SwiperSlide>  <div className='flex gap-3 justify-between'><p>jhfjyrekfhkjhewuywiuykejhgejhgiyeiuwrhgkheguyugyerkhgkerjhkj</p><img src={img1} className='bg-[#bab8b8]' /> </div>        </SwiperSlide>
             <SwiperSlide>  <div className='flex gap-3 justify-between '><p>jhfjyrekfhkjhewuywiuykejhgejhgiyeiuwrhgkheguyugyerkhgkerjhkj</p><img src={img1} /> </div>        </SwiperSlide>
             <SwiperSlide>  <div className='flex gap-3 justify-between'><p>jhfjyrekfhkjhewuywiuykejhgejhgiyeiuwrhgkheguyugyerkhgkerjhkj</p><img src={img1} /> </div>        </SwiperSlide>
             <SwiperSlide>  <div className='flex gap-3 justify-between'><p>jhfjyrekfhkjhewuywiuykejhgejhgiyeiuwrhgkheguyugyerkhgkerjhkj</p><img src={img1} /> </div>        </SwiperSlide>
@@ -43,7 +45,7 @@ function Slider() {
             <SwiperSlide>  <div className='flex gap-3 justify-between'><p>jhfjyrekfhkjhewuywiuykejhgejhgiyeiuwrhgkheguyugyerkhgkerjhkj</p><img src={img1} /> </div>        </SwiperSlide>
 
         </Swiper>
-        // </div>
+         </div>
     )
 }
 
